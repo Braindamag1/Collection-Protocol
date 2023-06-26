be9ff2d3b599cac771f7833d2e678cd7cdc0007d
YJ.Li <yuanjun.li@beyond-power.com>
2023-06-26 18:36:40 +0800

    feat: split on sequence and lazy evolution on sequence

--- Array,ArraySlice,Collection.playground/Sequence & Iterator.playground/Contents.swift
+++ Array,ArraySlice,Collection.playground/Sequence & Iterator.playground/Contents.swift
@@ -29,9 +29,28 @@ final class ReadRandom: IteratorProtocol {
     }
 }
 
-
-let randomSequence = AnySequence({ReadRandom()})
-randomSequence.prefix(10).map({print($0)})
+let randomSequence = AnySequence({ ReadRandom() })
 
 // sequence doesn't have notion of index
 
+extension Sequence {
+    func split(batchSize: Int) -> AnySequence<[Iterator.Element]> {
+        return AnySequence {
+            return AnyIterator {
+                var batch: [Iterator.Element] = []
+                var iterator = self.makeIterator()
+                while batch.count < batchSize, let elment = iterator.next() { // while let 也可以解包，头一次看到
+                    batch.append(elment)
+                }
+                return batch.isEmpty ? nil : batch
+            }
+        }
+    }
+}
+
+randomSequence.split(batchSize: 3).prefix(4)
+
+// randomSequence.split(batchSize: 3).map { "\($0)" } // runs indefinitely
+// This happens because the return type of map is an array, and arrays always have all their elements precomputed.
+
+let a = randomSequence.split(batchSize: 3).lazy.map({"\($0)"})
